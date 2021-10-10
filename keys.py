b6714fdf0ce392e92d3132ef5d953bbaa5bb858b
mcozar99 <m.cozar@alumnos.upm.es>
2021-10-10 20:15:21 +0100

    Deleted keys

--- keys.py
+++ keys.py
@@ -1,7 +1,7 @@
-ID = '07fe4ec097234922a4efa249506fba5c'
-ID_SECRET = 'ca0d858f38b44e9faba16433a7f69697'
+ID = 'xxxxxxxxxxxxxxxxxxxxxxxx'
+ID_SECRET = 'xxxxxxxxxxxxxxxxxxxxxxxx'
 redirect_uri = 'http://localhost:3000'
-genius_token = 'nU0CrZkK52ef7QyfWLZmPkXmFAyvmdRvtbdFXLipB4CDUBn4cyU49NHYJORukXym'
+genius_token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 scope = "user-library-read user-follow-read user-top-read playlist-read-private " \
         "user-read-currently-playing user-read-recently-played"
 
